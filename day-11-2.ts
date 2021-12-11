2fe4df6a1ab1fa94c05ed62b24030fa9faf8c879
Hugo Martinez <hugo.83300@gmail.com>
2021-12-11 08:07:59 +0100

    advent of code day 11

---
+++ day-11-2.ts
@@ -0,0 +1,62 @@
+import fs from 'fs';
+
+function getAdjacentCoordinates(x:number, y: number) {
+  return [[x, y + 1], [x + 1, y + 1],[x + 1, y], [x + 1, y - 1], [x, y - 1], [x - 1, y - 1], [x - 1, y], [x - 1, y + 1]];
+};
+
+// function hasFlashingOctopus(octopuses: number[][]) {
+//   return !!octopuses.find(line => line.find(octopuses => octopuses > 9))
+// }
+
+const visualizeOctopuses = (bassin: number[][]) => {
+  return bassin.map(line => line.reduce((prev, value) =>  value === 0 ? prev +=`\x1b[1;31m${value}\x1b[0m` : prev += `${value}`, '')).join('\n');
+}
+
+function day11_1() {
+  const data = fs.readFileSync('./inputs/day-11.txt', 'utf8');
+  const formatedData = data.split(/(?:\r\n|\r|\n)/g).map(line => line.split('').map(number => parseInt(number)));
+  let allOctopusFlashesStep:number | undefined = undefined;
+
+  for(let step = 0; step < 300; step++) {
+    let count = 0;
+    let hasFlashingOctopus = false;
+
+    formatedData.map((line, y) => line.map((octopus, x) => {
+      formatedData[y][x]++
+      if(formatedData[y][x] > 9) {
+        hasFlashingOctopus = true;
+      }
+    }));
+   
+    while (hasFlashingOctopus) {
+      hasFlashingOctopus = false;
+
+      formatedData.map((line, y) => line.map((octopus, x) => {
+        if(formatedData[y][x] > 9) {
+          count++;
+          getAdjacentCoordinates(x, y).map((coordinates => {
+            let element = formatedData[coordinates[1]]?.[coordinates[0]]
+            if(element) {
+              element++;
+              formatedData[coordinates[1]][coordinates[0]] = element;
+              if(element > 9) {
+                hasFlashingOctopus = true;
+              }
+            }
+          }));
+          formatedData[y][x] = 0;
+        }
+      }));
+    }
+
+    if(count === 100) {
+      allOctopusFlashesStep = step + 1;
+      break;
+    }
+  }
+ 
+  return allOctopusFlashesStep;
+}
+
+
+process.stdout.write(`${day11_1()}`);
